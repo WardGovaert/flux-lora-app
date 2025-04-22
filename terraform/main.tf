3f7f00482276f50f62e0c89cf4383655fda09571
Govaert Ward <ward.govaert2@student.howest.be>
2025-04-22 15:17:44 +0200

    update terraform main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,16 @@
 provider "google" {
-  project = var.project
+  project = "pure-courier-453515-t8"
   region  = "europe-west1"
-  zone    = "europe-west1-b"
 }
 
-resource "google_compute_instance" "lora_trainer" {
+resource "google_compute_instance" "trainer" {
   name         = "lora-trainer"
-  machine_type = "n1-standard-4"
+  machine_type = "n1-standard-8"
   zone         = "europe-west1-b"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-12"
+      image = "ubuntu-2004-focal"
     }
   }
 
@@ -20,9 +19,21 @@ resource "google_compute_instance" "lora_trainer" {
     access_config {}
   }
 
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    echo "Starting LoRA training setup..."
-    # Add your startup commands here
-  EOT
+  guest_accelerator {
+    type  = "nvidia-tesla-t4"
+    count = 1
+  }
+
+  metadata {
+    startup-script = <<-EOT
+      #!/bin/bash
+      apt-get update
+      apt-get install -y docker.io
+      docker run -d -p 8000:8000 your-docker-image
+    EOT
+  }
+
+  service_account {
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
 }
